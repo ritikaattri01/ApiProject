43634ad57ade3ce4f6e09a75a874e006655fa546
Ritika Digra <ritikaattri8@gmail.com>
2023-02-18 16:56:48 +0530

    navigate to main activity

--- app/src/main/java/com/app/apiproject/ui/SplashActivity.kt
+++ app/src/main/java/com/app/apiproject/ui/SplashActivity.kt
@@ -1,15 +1,22 @@
 package com.app.apiproject.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.app.apiproject.MainActivity
 import com.app.apiproject.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
+        navigateToMainActivity()
         Log.e("lifeCycle", "onCreate: ")
     }
 
@@ -41,5 +48,18 @@ class SplashActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         Log.e("lifeCycle", "onRestart: ")
+    }
+
+    fun navigateToMainActivity() {
+        lifecycleScope.launch {
+            delay(DELAY)
+            val i = Intent(applicationContext, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+    }
+
+    companion object {
+        const val DELAY = 1000L
     }
 }
